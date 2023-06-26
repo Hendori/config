36841a3fe3d6b4425d268fa128715b36bb87e7d2
hendri eleveld <hendori@gmail.com>
2023-06-26 20:56:07 +0200

    fix: cleaned up scripts for i3

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,10 +1,10 @@
 scrot /tmp/screen.png
-#files=(~/Wallpaper/*)
-#random_file="${files[RANDOM % ${#files[@]}]}"
-#printf "%s\n" ${random_file}
-#magick ${random_file} -paint 1 /tmp/screen.png
-magick /tmp/screen.png -spread 3 /tmp/screen.png
-#magick /tmp/screen.png -blend random_file /tmp/screen.png
+files=(~/Wallpaper/*)
+random_file="${files[RANDOM % ${#files[@]}]}"
+printf "%s\n" ${random_file}
+#magick ${random_file} -charcoal 9 -filter Hanning /tmp/screen.png
+magick /tmp/screen.png  -spread 1 -paint 1  /tmp/screen.png
+#magick /tmp/screen.png -blend ${random_file} /tmp/screen.png
 
 #lock screen
 i3lock -e -f -c 000000 -i /tmp/screen.png
